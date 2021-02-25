a728b4211101c861024f9833863809766b370ce5
Shawn Goertzen <sgoertzen@gmail.com>
2021-02-24 18:04:54 -0800

    Disclaimer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { ChoreList } from './ChoreList';
+import { Disclaimer } from './Disclaimer';
 
 // Note, either one of these work.  Don't understand the difference yet.
 //import * as data from "./data.json";
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <ChoreList days={data.days}/>
+      <Disclaimer/>
     </div>
   );
 }
---
+++ src/Disclaimer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Disclaimer = () => (
+    <div>
+        Morning walk before 12:30pm, Night walk before 6:30pm
+    </div>
+);
