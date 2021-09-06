aa9ccc9d19bde965624cdb9ec69e1f09ead2c730
Shawn Goertzen <sgoertzen@gmail.com>
2021-09-06 08:32:07 -0700

    adding rm's bday

--- src/components/BirthdayConfetti.tsx
+++ src/components/BirthdayConfetti.tsx
@@ -6,6 +6,7 @@ export const isBirthday = (day:Date) => {
     return ((getDate(day) === 25 && getMonth(day) === 2) ||
         (getDate(day) === 9 && getMonth(day) === 3) ||
         (getDate(day) === 29 && getMonth(day) === 6) ||
+        (getDate(day) === 6 && getMonth(day) === 8) ||
         (getDate(day) === 13 && getMonth(day) === 8))
 }
 
